4fbe7967d823af6bb13e2b4d5e3b1bcc320f1312
cedriclemercier <cedric.lemercier@outlook.com>
2020-09-08 09:54:12 +1000

    Added hotjar plugin and graphql inline images REFRACTORING

--- src/components/seo.js
+++ src/components/seo.js
@@ -87,21 +87,6 @@ function SEO({ description, lang, meta, title, yoast }) {
       ].concat(meta)}
     >
       <link rel="icon" href={wp.seo.schema.logo.mediaItemUrl}></link>
-
-      {/* Analytics */}
-      {(function (h, o, t, j, a, r) {
-        h.hj =
-          h.hj ||
-          function () {
-            ;(h.hj.q = h.hj.q || []).push(arguments)
-          }
-        h._hjSettings = { hjid: 1978402, hjsv: 6 }
-        a = o.getElementsByTagName("head")[0]
-        r = o.createElement("script")
-        r.async = 1
-        r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv
-        a.appendChild(r)
-      })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=")}
     </Helmet>
   )
 }
